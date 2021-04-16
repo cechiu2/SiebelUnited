12a24d51b69025c3b50f754dd3e20b77e751b1ef
cechiu2 <cechiu2@illinois.edu>
2021-04-15 20:52:20 -0500

    Here's our Android Studio project

---
+++ app/src/main/java/siebelunited/cs465/illinois/edu/focus/MainActivity.java
@@ -0,0 +1,13 @@
+package siebelunited.cs465.illinois.edu.focus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
